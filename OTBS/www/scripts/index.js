0e48649b4abf189aa54b6162e089c1cee964b070
Liang Xin <liangxin4812@gmail.com>
2018-11-06 15:45:06 +0800

    Ready to use

--- OTBS/www/scripts/index.js
+++ OTBS/www/scripts/index.js
@@ -1,30 +1,72 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
+﻿(function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    var userid;
+    var password;
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    };
+    $(document).ready(function () {
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+        $("#LoginForm").validate({
+            messages: {
+                txtLogin: "User ID is required",
+                txtPassword: "Password is required",
+            },
+            focusInvalid: false,
+            submitHandler: function () {
+                return false;
+            },
+            errorPlacement: function (error, element) {
+                error.appendTo(element.parent().parent().after());
+            },
+        });
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
+        $("#btnLogin").bind("click", function () {
+            if ($("#LoginForm").valid()) {
+                //login();
+                window.location = "makereservation.html";
+            }
+        });
+
+        $("#btnNewUser").bind("click", function () {
+            window.location = "newuser.html";
+        });
+
+    }); 
+
+    function login() {
+        var url = serverURL() + "/login.php";
+        var result;
+        userid = $("#txtLogin").val();
+        password = $("#txtPassword").val();
+
+        var JSONObject = {
+            "userid": userid,
+            "password": password
+        };
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            data: JSONObject,
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            success: function (arr) {
+                _getLoginResult(arr);
+            },
+            error: function () {
+                validationMsg();
+            }
+        });
+    }
+
+    function _getLoginResult(arr) {
+        if (arr[0].result.trim() !== "0") {
+            localStorage.setItem("userid", userid);
+            localStorage.setItem("password", password);
+            validationMsgs("Login OK", "Information", "OK");
+        }
+        else {
+            validationMsgs("Error in Username or Password", "Validation", "Try Again");
+        }
+    }
 } )();
---
+++ OTBS/www/scripts/makereservation.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+
+    $(document).ready(function () {
+        $("#btntestfunction").bind("click", function () {
+
+            alert("hello");
+            window.location = "index.html";
+        });
+    });
+
+})();
