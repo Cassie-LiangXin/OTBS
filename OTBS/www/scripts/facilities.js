245de5930a16fa8d23721e1ce5eb4c456ff9db56
Liang Xin <liangxin4821@gmail.com>
2018-12-24 21:39:06 +0800

    241218/2138

--- OTBS/www/scripts/facilities.js
+++ OTBS/www/scripts/facilities.js
@@ -1,11 +1,20 @@
 ﻿
 (function () {
     "use strict";
-
+    var button;
+    var link;
     $(document).ready(function () {
         $("#gotocontactus").bind("click", function () {
 
             window.location = "contactus.html";
+        });
+
+        $("#facilityformcheck").bind("click", function () {
+            alert();
+            button = document.getElementById("facilityformcheck");
+            link = document.getElementById("facilitydialoglink");
+            button.style.display = "block";
+            link.style.display = "none";
         });
     });
 
---
+++ OTBS/www/scripts/housekeeping.js
@@ -0,0 +1,26 @@
+﻿
+(function () {
+    "use strict";
+    var Bed;
+    var BathTowels;
+    var HandTowels;
+    var Sofa;
+    var Amenity;
+
+
+    $(document).ready(function () {
+
+        $("#gotocontactus").bind("click", function () {
+
+            window.location = "contactus.html";
+        });
+
+       
+    });
+
+
+ 
+
+
+
+    });
