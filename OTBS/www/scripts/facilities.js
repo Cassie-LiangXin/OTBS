d69134bbd88dbba4e89cdf92628fc1f79ecb5e96
Liang Xin <liangxin4821@gmail.com>
2018-12-24 22:46:25 +0800

    241218/2246

--- OTBS/www/scripts/facilities.js
+++ OTBS/www/scripts/facilities.js
@@ -4,18 +4,7 @@
     var button;
     var link;
     $(document).ready(function () {
-        $("#gotocontactus").bind("click", function () {
 
-            window.location = "contactus.html";
-        });
-
-        $("#facilityformcheck").bind("click", function () {
-            alert();
-            button = document.getElementById("facilityformcheck");
-            link = document.getElementById("facilitydialoglink");
-            button.style.display = "block";
-            link.style.display = "none";
-        });
     });
 
 })();
--- OTBS/www/scripts/housekeeping.js
+++ OTBS/www/scripts/housekeeping.js
@@ -1,26 +1,66 @@
-﻿
-(function () {
+﻿(function() {
     "use strict";
-    var Bed;
-    var BathTowels;
-    var HandTowels;
-    var Sofa;
-    var Amenity;
+    var bed=1;
+    var towel = 1;
+    var sofa = 1;
+    var amenity = 1;
 
 
     $(document).ready(function () {
-
-        $("#gotocontactus").bind("click", function () {
-
-            window.location = "contactus.html";
+        //bed
+        $("#addbed").bind("click", function () { 
+            bed = bed + 1;
+            document.getElementById("bedvalue").value = bed;
         });
 
-       
+        $("#removebed").bind("click", function () {
+            bed = bed - 1;
+            if (bed <= 0) { bed=1; }
+            document.getElementById("bedvalue").value = bed;
+
+        });
+        //towel
+        $("#addtowel").bind("click", function () {
+            towel = towel + 1;
+            document.getElementById("towelvalue").value = towel;
+        });
+
+        $("#removetowel").bind("click", function () {
+            towel = towel - 1;
+            if (towel <= 0) { towel = 1; }
+            document.getElementById("towelvalue").value = towel;
+        });
+        //sofa
+        $("#addsofa").bind("click", function () {
+            sofa = sofa + 1;
+            document.getElementById("sofavalue").value = sofa;
+        });
+
+        $("#removesofa").bind("click", function () {
+            sofa = sofa - 1;
+            if (sofa <= 0) { sofa = 1; }
+            document.getElementById("sofavalue").value = sofa;
+        });
+        //amenity
+        $("#addamenity").bind("click", function () {
+            amenity = amenity + 1;
+            document.getElementById("amenityvalue").value = amenity;
+        });
+
+        $("#removeamenity").bind("click", function () {
+            amenity = amenity - 1;
+            if (amenity <= 0) { amenity = 1; }
+            document.getElementById("amenityvalue").value = amenity;
+        });
+
+
+
+
+
+
     });
 
 
- 
 
 
-
-    });
+})();
